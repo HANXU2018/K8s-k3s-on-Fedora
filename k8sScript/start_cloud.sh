856e5511d6e457bde32dc723bb94e68d2a0325d8
HANXU2018 <1076998404@qq.com>
2020-08-31 12:42:11 +0800

    Update start_cloud.sh

--- k8sScript/start_cloud.sh
+++ k8sScript/start_cloud.sh
@@ -63,15 +63,53 @@ ssh master curl -fsSL https://get.docker.com | bash -s docker
 ssh master systemctl daemon-reload
 ssh master systemctl restart docker.service
 
-#install k3sup
-ssh master curl -sLS https://get.k3sup.dev | sh
-#k3sup is a tool for installing K3S
-#install k3s server
-echo "master install"
+ssh master cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+# Set SELinux in permissive mode (effectively disabling it)
+ssh master sudo setenforce 0
+ssh master sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-ssh master k3sup install --ip $MASTER_IP --user root
+ssh master sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
-#Install K3S remotely using k3sup
+ssh master sudo systemctl enable --now kubelet
+ssh master sudo yum install -y ipvsadm
+
+ssh master cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-aarch64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+ssh master yum clean all
+ssh master yum install kubernetes-cni -y
+
+ssh master echo"FLANNEL_NETWORK=10.244.0.0/16
+FLANNEL_SUBNET=10.224.0.1/24
+FLANNEL_MTU=1450
+FLANNEL_IPMASQ=true">/run/flannel/subnet.env
+
+ssh master systemctl daemon-reload
+ssh master systemctl restart docker.service
+ssh master systemctl enable docker
+ssh master systemctl enable kubelet.service
+ssh master systemctl start docker
+ssh master systemctl start kubelet
+
+# node
+
+
+#ssh blue if [ $(hostname) != "blue" ]; then hostnamectl set-hostname blue  ;fi
 ssh blue hostnamectl set-hostname blue
 #install docker
 ssh blue curl -fsSL https://get.docker.com | bash -s docker
@@ -80,29 +118,108 @@ ssh blue curl -fsSL https://get.docker.com | bash -s docker
 #start docker
 ssh blue systemctl daemon-reload
 ssh blue systemctl restart docker.service
-echo "blue install"
-#install k3sup
-ssh blue curl -sLS https://get.k3sup.dev | sh
-# install k3s agent blue
 
-k3sup join --ip $BLUE_IP --server-ip $MASTER_IP --user root
-# Install K3S remotely using k3sup
-echo "green install"
-#Install K3S remotely using k3sup
-ssh green hostnamectl set-hostname green
+ssh blue cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+# Set SELinux in permissive mode (effectively disabling it)
+ssh blue sudo setenforce 0
+ssh blue sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+ssh blue sudo yum install -y kubelet kubeadm  --disableexcludes=kubernetes
+
+ssh blue sudo systemctl enable --now kubelet
+ssh blue sudo yum install -y ipvsadm
+
+ssh blue cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-aarch64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+ssh blue yum clean all
+ssh blue yum install kubernetes-cni -y
+
+ssh blue echo"FLANNEL_NETWORK=10.244.0.0/16
+FLANNEL_SUBNET=10.224.0.1/24
+FLANNEL_MTU=1450
+FLANNEL_IPMASQ=true">/run/flannel/subnet.env
+
+ssh blue systemctl daemon-reload
+ssh blue systemctl restart docker.service
+ssh blue systemctl enable docker
+ssh blue systemctl enable kubelet.service
+ssh blue systemctl start docker
+ssh blue systemctl start kubelet
+
+#ssh blue if [ $(hostname) != "blue" ]; then hostnamectl set-hostname blue  ;fi
+ssh blue hostnamectl set-hostname blue
 #install docker
-ssh green curl -fsSL https://get.docker.com | bash -s docker
+ssh blue curl -fsSL https://get.docker.com | bash -s docker
 # As a container, Docker is installed on all servers using installation scripts
 
 #start docker
 ssh green systemctl daemon-reload
 ssh green systemctl restart docker.service
 
-#install k3sup
-ssh green curl -sLS https://get.k3sup.dev | sh
-# install k3s agent blue
-k3sup join --ip $GREEN_IP --server-ip $MASTER_IP --user root
-# Install K3S remotely using k3sup
+ssh green cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+# Set SELinux in permissive mode (effectively disabling it)
+ssh green sudo setenforce 0
+ssh green sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+ssh green sudo yum install -y kubelet kubeadm  --disableexcludes=kubernetes
+
+ssh green sudo systemctl enable --now kubelet
+ssh green sudo yum install -y ipvsadm
+
+ssh green cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-aarch64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+ssh green yum clean all
+ssh green yum install kubernetes-cni -y
+
+ssh green echo"FLANNEL_NETWORK=10.244.0.0/16
+FLANNEL_SUBNET=10.224.0.1/24
+FLANNEL_MTU=1450
+FLANNEL_IPMASQ=true">/run/flannel/subnet.env
+
+ssh green systemctl daemon-reload
+ssh green systemctl restart docker.service
+ssh green systemctl enable docker
+ssh green systemctl enable kubelet.service
+ssh green systemctl start docker
+ssh green systemctl start kubelet
+
+
+
+ssh master git clone https://github.com/HANXU2018/kubernetes_f30_demo/
+ssh master cd kubernetes_f30_demo
+ssh master sh start_cloud.sh
 
 echo ""
 echo "Checking nodes"
